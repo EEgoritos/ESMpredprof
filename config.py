9f09dbd18ce51c6f2f7a7258f2050c526dfb7d4b
pyataninastesha <pyataninastefa@yandex.ru>
2025-01-21 22:27:48 +0300

    готовый вход и регистрация

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-SECRET_KEY = 'your_secret_key'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-UPLOAD_FOLDER = 'app/uploads'
-AUDIO_FOLDER = 'app/static/audio'
--- run.py
+++
@@ -1,8 +0,0 @@
-from app import create_app
-
-# Создаем экземпляр приложения
-app = create_app()
-
-if __name__ == '__main__':
-    # Запускаем приложение в режиме отладки
-    app.run(debug=True)
